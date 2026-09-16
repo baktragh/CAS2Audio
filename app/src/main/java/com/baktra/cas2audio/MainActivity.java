8c0f816569ee4c7bc4db80d3e72d9910521d230a
baktra <zylon@post.cz>
2026-09-16 09:56:18 +0200

    Graphics: Refine cassette graphics for both bright and dark themes

--- app/src/main/java/com/baktra/cas2audio/MainActivity.java
+++ app/src/main/java/com/baktra/cas2audio/MainActivity.java
@@ -97,7 +97,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         /*Set the title*/
-        setTitle("CAS2Audio 1.0.6-test-02");
+        setTitle("CAS2Audio 1.0.6-test-03");
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
